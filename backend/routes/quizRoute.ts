e517fab0d6610957708d6c94636d6c252e60f4e6
VirangaWeerabandara <umiduviranga8@gmail.com>
2025-12-12 20:36:22 +0530

    style: format import statements and route definitions for improved readability

--- backend/routes/quizRoute.ts
+++ backend/routes/quizRoute.ts
@@ -1,41 +1,39 @@
 import { Router } from "express";
-import { checkQuizzesPublishedStatus, getLeaderBoard, getQuizWithQuestions, publishAllQuizzes, submitQuiz, unpublishAllQuizzes, createQuizSet, getQuizSets, updateQuizSet } from "../controllers/quizController.js";
+import {
+  checkQuizzesPublishedStatus,
+  getLeaderBoard,
+  getQuizWithQuestions,
+  publishAllQuizzes,
+  submitQuiz,
+  unpublishAllQuizzes,
+  createQuizSet,
+  getQuizSets,
+  updateQuizSet,
+} from "../controllers/quizController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { adminOnly } from "../middleware/adminAuthMiddleware.js";
 
-
 const QuizRouter = Router();
 
+QuizRouter.route("/submit-quiz").post(protect, submitQuiz);
 
+QuizRouter.route("/get-leaderboard").get(getLeaderBoard);
 
-QuizRouter.route("/submit-quiz")
-  .post( protect , submitQuiz);
+QuizRouter.route("/check-quiz-published-status").get(
+  protect,
+  checkQuizzesPublishedStatus
+);
 
-QuizRouter.route("/get-leaderboard")
-  .get(getLeaderBoard);
+QuizRouter.route("/publish-all-quizzes").post(adminOnly, publishAllQuizzes);
 
+QuizRouter.route("/unpublish-all-quizzes").post(adminOnly, unpublishAllQuizzes);
 
-QuizRouter.route("/check-quiz-published-status")
-.get(checkQuizzesPublishedStatus);
+QuizRouter.route("/get-quiz-sets").get(protect, getQuizSets);
 
-
-QuizRouter.route("/publish-all-quizzes")
-.post(adminOnly, publishAllQuizzes);
-
-QuizRouter.route("/unpublish-all-quizzes")
-.post(adminOnly, unpublishAllQuizzes);
-
-QuizRouter.route("/get-quiz-sets")
-  .get(getQuizSets);
-
-QuizRouter.route("/create-quiz-set")
-  .post(adminOnly, createQuizSet);
-
-
+QuizRouter.route("/create-quiz-set").post(adminOnly, createQuizSet);
 
 QuizRouter.route("/:quizId")
   .get(getQuizWithQuestions)
   .patch(adminOnly, updateQuizSet);
 
 export default QuizRouter;
-
